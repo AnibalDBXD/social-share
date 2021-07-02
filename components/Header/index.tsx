8435309372a7050b57990f969c2db2bdd238e816
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-02 16:52:03 -0400

    header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Heading, Text, Box } from '@chakra-ui/react';
+
+const Header = () => (
+  <Box textAlign="center">
+    <Heading color="blue.700">
+      Social Share
+    </Heading>
+    <Text fontSize="1.2rem" color="gray.500">
+      Share all your publications on all networks!
+    </Text>
+  </Box>
+);
+
+export default Header;
