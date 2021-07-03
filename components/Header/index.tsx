0354a58584dea9963c398eaed2d30a5095e501b3
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-03 18:45:15 -0400

    Fix header bottom text

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,7 +6,7 @@ const Header = () => (
       Social Share
     </Heading>
     <Text fontSize="1.2rem" color="gray.500">
-      Share all your publications on all networks!
+      Share all your publications on all networks
     </Text>
   </Box>
 );
