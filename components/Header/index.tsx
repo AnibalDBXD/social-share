5b44ec278cb53e76dbfe9518b3376d42e9a75f91
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-04 16:02:24 -0400

    Add color mode on header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,21 @@
-import { Heading, Text, Box } from '@chakra-ui/react';
+import {
+  Heading, Text, Box, useColorModeValue,
+} from '@chakra-ui/react';
 
-const Header = () => (
-  <Box textAlign="center">
-    <Heading color="blue.700">
-      Social Share
-    </Heading>
-    <Text fontSize="1.2rem" color="gray.500">
-      Share all your publications on all networks
-    </Text>
-  </Box>
-);
+const Header = () => {
+  const headingColor = useColorModeValue('blue.700', 'blue.400');
+  const textColor = useColorModeValue('gray.500', 'gray.300');
+  return (
+
+    <Box textAlign="center">
+      <Heading color={headingColor}>
+        Social Share
+      </Heading>
+      <Text fontSize="1.2rem" color={textColor}>
+        Share all your publications on all networks
+      </Text>
+    </Box>
+  );
+};
 
 export default Header;
