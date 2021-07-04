3cff49437558bbf1998e171116b20456020e5147
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-04 14:18:33 -0400

    Add Email, linkedin, reddit, whatsapp share button

--- components/ShareList/index.tsx
+++ components/ShareList/index.tsx
@@ -1,5 +1,16 @@
 import {
-  TwitterShareButton, TwitterIcon, FacebookShareButton, FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  EmailShareButton,
+  EmailIcon,
+  LinkedinIcon,
+  LinkedinShareButton,
+  RedditShareButton,
+  RedditIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
 } from 'react-share';
 import { HStack } from '@chakra-ui/react';
 import { IShareList } from './types';
@@ -8,10 +19,18 @@ const ShareList = ({
   description, hashTags, title, errors, url,
 }: IShareList) => (
   <HStack spacing={4}>
+    <EmailShareButton
+      subject={title}
+      body={description}
+      url={url}
+      disabled={errors.title?.type !== undefined}
+    >
+      <EmailIcon />
+    </EmailShareButton>
     <TwitterShareButton
       title={title}
       hashtags={hashTags}
-      url={description}
+      url={description + url}
       disabled={errors.title?.type !== undefined}
     >
       <TwitterIcon />
@@ -21,10 +40,32 @@ const ShareList = ({
       quote={`${title} \n ${description}
     `}
       disabled={errors.title?.type !== undefined || url === ''}
-      hashtag={hashTags.join(' ')}
+      hashtag={hashTags.map((value) => (`#${value}`)).join(' ')}
     >
       <FacebookIcon />
     </FacebookShareButton>
+    <LinkedinShareButton
+      title={title}
+      summary={description}
+      url={url}
+      disabled={errors.title?.type !== undefined}
+    >
+      <LinkedinIcon />
+    </LinkedinShareButton>
+    <RedditShareButton
+      title={title}
+      url={`${description} ${url}`}
+      disabled={errors.title?.type !== undefined}
+    >
+      <RedditIcon />
+    </RedditShareButton>
+    <WhatsappShareButton
+      title={title}
+      url={`${description} ${url}`}
+      disabled={errors.title?.type !== undefined}
+    >
+      <WhatsappIcon />
+    </WhatsappShareButton>
   </HStack>
 );
 
